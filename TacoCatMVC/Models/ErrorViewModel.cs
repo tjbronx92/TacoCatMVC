7c4110c969e51f8752c67c2d94fb55ac4924a756
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-06-09 10:13:43 -0700

    Getting Started

---
+++ TacoCatMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TacoCatMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ TacoCatMVC/Models/Palindrome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TacoCatMVC.Models
+{
+	public class Palindrome
+	{
+		public string InputWord { get; set; }
+		public string RevWord { get; set; }
+		public bool IsPalindrome { get; set; }
+		public string Message { get; set; }
+	}	
+}
+
